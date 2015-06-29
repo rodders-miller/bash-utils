cbf96f99f6ae5ad9735371fbc1326993f1f5706a
rmiller <rmiller@hp-fedora-20.home>
2015-06-30 00:09:00 +0100

    Added filesize print to user and posisbility of compression using a trick of coverting to ps and then back to pdf

--- printer/scan-doc.sh
+++ printer/scan-doc.sh
@@ -29,6 +29,22 @@ done
 echo "Aggregating to single pdf $COUNTER page(s) to ~/output.pdf (please mv before re-running";
 pdftk $SCAN_DIR/* output ~/output.pdf;
 
+# Get the file size of the output using the du and cut commands:
+filesize=$(du -k ~/output.pdf | cut -f1)
+
+echo "Would you like the pdf to be compressed (file size is $filesize)? (y for yes)"
+read OPTIONS
+
+if [ "$OPTIONS" = "y" ]; then
+  #attempt compressions by converting to large ps file and then back to pdf
+  pdf2ps ~/output.pdf ~/output.ps
+  ps2pdf ~/output.ps ~/output.pdf
+  rm ~/output.ps
+
+  filesize=$(du -k ~/output.pdf | cut -f1)
+  echo "Compressed file size is now $filesize)"
+
+fi
 
 
  
